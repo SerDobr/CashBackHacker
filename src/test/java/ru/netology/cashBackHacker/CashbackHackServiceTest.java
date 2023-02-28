901a74f76d884397d8d810fa61f9d3988730c067
Neto test <test@neto.com>
2023-02-28 23:43:27 +0300

    TestNG

--- src/test/java/ru/netology/cashBackHacker/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashBackHacker/CashbackHackServiceTest.java
@@ -1,9 +1,9 @@
 package ru.netology.cashBackHacker;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
+
 
 class CashbackHackServiceTest {
 
@@ -12,7 +12,7 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int expected = 100;
         int actual = service.remain(900);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
 
     }
@@ -22,9 +22,18 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int expected = 900;
         int actual = service.remain(100);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
 
     }
 
+    @Test
+    void shouldCashback1001() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1001);
+        assertEquals(actual, expected);
+
+
+    }
 }
